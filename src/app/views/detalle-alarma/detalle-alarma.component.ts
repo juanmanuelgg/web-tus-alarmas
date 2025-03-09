829c630a8e0929ac21f0c8e3e9a9ea9d6bb458bc
Juan Manuel González Garzón <jm.gonzalez1844@uniandes.edu.co>
2025-03-09 06:59:25 -0500

    Detalle de una alarma.

--- src/app/views/detalle-alarma/detalle-alarma.component.ts
+++ src/app/views/detalle-alarma/detalle-alarma.component.ts
@@ -1,19 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { AlarmasService } from '../../services/alarmas/alarmas.service';
 import Alarma from '../../services/alarmas/alarma';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-detalle-alarma',
-  imports: [CommonModule],
+  imports: [CommonModule, MatSlideToggleModule, ReactiveFormsModule],
   templateUrl: './detalle-alarma.component.html',
   styleUrl: './detalle-alarma.component.scss',
 })
 export class DetalleAlarmaComponent implements OnInit {
-  modoCreacion = true;
   alarma?: Alarma;
+
+  detalleAlarmaForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]),
+    image: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/(http(s?):)([/|.|\w|\s|-])*(:[0-9]*)?([/|.|\w|\s|-])*\.(?:jpg|gif|png|svg)/),
+    ]),
+    alarms: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^([01]?[0-9]|2[0-3]):[0-5][0-9](,([01]?[0-9]|2[0-3]):[0-5][0-9])*$/
+      ),
+    ]),
+    active: new FormControl(false, []),
+  });
 
   private readonly route = inject(ActivatedRoute);
   private readonly router = inject(Router);
@@ -24,11 +49,16 @@ export class DetalleAlarmaComponent implements OnInit {
   ngOnInit(): void {
     const alarmId = this.route.snapshot.paramMap.get('id');
     if (alarmId && Number.isInteger(+alarmId)) {
-      this.modoCreacion = false;
-
       this.alarmasService.getAlarma(alarmId).subscribe({
         next: (alarma) => {
           this.alarma = alarma;
+          this.detalleAlarmaForm.setValue({
+            name: alarma.name,
+            description: alarma.description,
+            image: alarma.image,
+            alarms: alarma.alarms.join(','),
+            active: alarma.active,
+          });
         },
         error: (error) => {
           console.error('Error al obtener la alarma', error);
@@ -37,4 +67,13 @@ export class DetalleAlarmaComponent implements OnInit {
       });
     } // fi
   }
+
+  public editAlarma(): void {
+    if (this.detalleAlarmaForm.valid) {
+    }
+  }
+
+  public createsAlarma(): void {}
+
+  public deleteAlarma(): void {}
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,16 +1,47 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet, Router } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  RouterModule,
+  RouterOutlet,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet],
+  imports: [RouterModule, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  listAlarms = 'listbtn';
+  listUbications = 'listbtn';
+  listReports = 'listbtn';
+
   private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.route.url.subscribe(() => {
+      this.listAlarms = 'listbtn';
+      this.listUbications = 'listbtn';
+      this.listReports = 'listbtn';
+
+      const lastPartHref = window.location.href.split('/').pop();
+      switch (lastPartHref) {
+        case 'lista-alarmas':
+          this.listAlarms = 'listbtn-active';
+          break;
+        case 'lista-ubicaciones':
+          this.listUbications = 'listbtn-active';
+          break;
+        case 'reportes':
+          this.listReports = 'listbtn-active';
+          break;
+      }
+    });
+  }
 
   public logout() {
     localStorage.removeItem('email');
